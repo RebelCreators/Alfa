bb917d7c58730ca8b550c2d9c0cdb1580e39a517
lorenzo stanton <lstanii54@gmail.com>
2018-05-13 01:04:42 -0700

    Fixed about us alignment

--- Modules/Alfa/docs.sh
+++ Modules/Alfa/docs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 rm -Rf docs && jsdoc -c jsdoc.conf
+
+browserify --standalone ethereumjs index.js > ethereumjs-wallet.bundle.js
+
+minify --output ethereumjs-wallet.bundle.min.js ethereumjs-wallet.bundle.js             
+
+
+qrcode -o testqrcode.svg "https://www.google.com/search?q=qr+code+generator&oq=qrcodegen&aqs=chrome.1.69i57j0l5.8295j0j7&sourceid=chrome&ie=UTF-8"
